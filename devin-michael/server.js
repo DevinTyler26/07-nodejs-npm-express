5844a715849a83e77dc8df4e50d2fb02348962d7
devintyler26 <devincunningham@allegropediatrics.com>
2017-12-19 19:05:44 -0800

    finished lab 7

--- devin-michael/server.js
+++ devin-michael/server.js
@@ -10,6 +10,8 @@ const app = express()
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
+//COMMENTED: They are located in a public folder so people who are on the site can see them. All files outside this folder are not visible to the public.
+
 app.use(express.static('./public'))
 
 app.post('/articles', bodyParser, function(request, response) {
@@ -18,10 +20,17 @@ app.post('/articles', bodyParser, function(request, response) {
   response.send('Record posted to server!!');
 })
 
+app.get('/new', (request, response) => {
+  response.sendFile('new.html', {root: './public'})
+})
+
 app.get('/', (request, response) => {
   response.send('Responding')
 })
 
+app.use((request, response) => response.status(404).sendFile('404.gif', {root: './public'}))
+
+
 app.listen(PORT, () =>{
-  console.log('Listening')
+  console.log(`Listening on port ${PORT}`)
 })
